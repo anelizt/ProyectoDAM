c9e236d1d88bcaf668ffe886f18febf8f4514ab5
Jesus Garcia <jesusmgp1988@gmail.com>
2025-04-16 16:29:08 +0200

    Entrenamientos creados en html, minijuego en modo horda

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const jwt = require('jsonwebtoken');
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.static(__dirname));
 
 const db = mysql.createConnection({
     host: 'localhost',
